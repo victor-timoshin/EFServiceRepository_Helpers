2a91189f16f2d78346803841cd799edd6b07974a
victor-timoshin <victor-timoshin@hotmail.com>
2015-09-12 11:45:01 +0300

    Initial commit

---
+++ EFServiceRepository_Helpers/GenericRepository.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace EFServiceRepository_Helpers
+{
+	public interface IGenericRepository<T>
+	{
+		IQueryable<T> GetAll();
+		Task<List<T>> GetAllAsync();
+		IQueryable<T> Find(Expression<Func<T, bool>> predicate);
+		Task<List<T>> FindAsync(Expression<Func<T, bool>> match);
+		T Single(Expression<Func<T, bool>> predicate);
+		Task<T> SingleAsync(Expression<Func<T, bool>> predicate);
+		T First(Expression<Func<T, bool>> predicate);
+		Task<T> FirstAsync(Expression<Func<T, bool>> predicate);
+		void Remove(T entity);
+		void Add(T entity);
+		void Update(T entity);
+		Task<int> CountAsync();
+	}
+
+	public class GenericRepository<T> : IGenericRepository<T> where T : class
+	{
+		private DbContext databaseContext;
+		private DbSet<T> entitySet;
+
+		/// <summary>
+		/// Конструктор класса
+		/// </summary>
+		/// <param name="context"></param>
+		public GenericRepository(DbContext context)
+		{
+			if (context == null)
+				throw new ArgumentNullException("context");
+
+			databaseContext = context;
+			entitySet = context.Set<T>();
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <returns></returns>
+		public IQueryable<T> GetAll()
+		{
+			return entitySet.AsQueryable();
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <returns></returns>
+		public async Task<List<T>> GetAllAsync()
+		{
+			return await entitySet.ToListAsync();
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="predicate"></param>
+		/// <returns></returns>
+		public IQueryable<T> Find(Expression<Func<T, bool>> predicate)
+		{
+			return entitySet.Where(predicate);
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="predicate"></param>
+		/// <returns></returns>
+		public async Task<List<T>> FindAsync(Expression<Func<T, bool>> match)
+		{
+			return await entitySet.Where(match).ToListAsync();
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="predicate"></param>
+		/// <returns></returns>
+		public T Single(Expression<Func<T, bool>> predicate)
+		{
+			return entitySet.SingleOrDefault(predicate);
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="predicate"></param>
+		/// <returns></returns>
+		public async Task<T> SingleAsync(Expression<Func<T, bool>> predicate)
+		{
+			return await entitySet.SingleOrDefaultAsync(predicate);
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="predicate"></param>
+		/// <returns></returns>
+		public T First(Expression<Func<T, bool>> predicate)
+		{
+			return entitySet.FirstOrDefault(predicate);
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="predicate"></param>
+		/// <returns></returns>
+		public async Task<T> FirstAsync(Expression<Func<T, bool>> predicate)
+		{
+			return await entitySet.FirstOrDefaultAsync(predicate);
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="entity"></param>
+		public void Add(T entity)
+		{
+			DbEntityEntry entry = databaseContext.Entry(entity);
+			if (entry.State == EntityState.Detached)
+				entitySet.Add(entity);
+			else
+				entry.State = EntityState.Added;
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="entity"></param>
+		public void Remove(T entity)
+		{
+			DbEntityEntry entry = databaseContext.Entry(entity);
+			if (entry.State == EntityState.Deleted)
+			{
+				entitySet.Attach(entity);
+				entitySet.Remove(entity);
+			}
+			else
+				entry.State = EntityState.Deleted;
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="entity"></param>
+		public void Update(T entity)
+		{
+			try
+			{
+				DbEntityEntry entry = databaseContext.Entry(entity);
+				if (entry.State == EntityState.Detached)
+					entitySet.Attach(entity);
+
+				entry.State = EntityState.Modified;
+			}
+			catch (DbUpdateConcurrencyException exception)
+			{
+				DbEntityEntry entry = exception.Entries.Single();
+				var clientValues = (T)entry.Entity;
+				var databaseValues = (T)entry.GetDatabaseValues().ToObject();
+
+				throw exception;
+			}
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <returns></returns>
+		public async Task<int> CountAsync()
+		{
+			return await entitySet.CountAsync();
+		}
+	}
+}
---
+++ EFServiceRepository_Helpers/UnitOfWork.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Core.Objects;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace EFServiceRepository_Helpers
+{
+	public interface IUnitOfWork : IDisposable
+	{
+		IGenericRepository<T> RepositoryFor<T>() where T : class;
+		void Commit();
+		Task<int> CommitAsync();
+	}
+
+	public class UnitOfWork : IUnitOfWork
+	{
+		//private static readonly ILog log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+		private DbContext context;
+		private ObjectContext objectContext;
+		private bool disposed;
+
+		/// <summary>
+		/// Конструктор класса
+		/// </summary>
+		/// <param name="context"></param>
+		public UnitOfWork(DbContext context)
+		{
+			if (context == null)
+				throw new ArgumentNullException("context");
+
+			this.context = context;
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <returns></returns>
+		public IGenericRepository<T> RepositoryFor<T>() where T : class
+		{
+			return new GenericRepository<T>(this.context);
+		}
+
+		/// <summary>
+		/// Сохраняет все обновления в источнике данных и сбрасывает отслеживание изменений в контексте объекта
+		/// </summary>
+		public void Commit()
+		{
+			bool saveFailed;
+			do
+			{
+				saveFailed = false;
+
+				try
+				{
+					this.context.SaveChanges();
+				}
+				catch (DbUpdateConcurrencyException exception)
+				{
+					//log.Debug("\n\n*** {0}\n\n", exception.InnerException);
+
+					saveFailed = true;
+					exception.Entries.Single().Reload();
+
+					throw exception;
+				}
+			} while (saveFailed);
+		}
+
+		/// <summary>
+		/// Сохраняет все обновления в источнике данных и сбрасывает отслеживание изменений в контексте объекта
+		/// Асинхронный метод
+		/// </summary>
+		public Task<int> CommitAsync()
+		{
+			try
+			{
+				return this.context.SaveChangesAsync();
+			}
+			catch (DbUpdateException exception)
+			{
+				//log.Debug("\n\n*** {0}\n\n", exception.InnerException);
+				throw exception;
+			}
+		}
+
+		#region Implementation of IDisposable
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public void Dispose()
+		{
+			if (this.context != null)
+				this.context.Dispose();
+
+			GC.SuppressFinalize(this);
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="disposing"></param>
+		public virtual void Dispose(bool disposing)
+		{
+			if (disposed)
+				return;
+
+			if (disposing)
+			{
+				try
+				{
+					if (objectContext != null && objectContext.Connection.State == ConnectionState.Open)
+						objectContext.Connection.Close();
+				}
+				catch (ObjectDisposedException)
+				{
+				}
+
+				//if (_dataContext != null)
+				//{
+				//    _dataContext.Dispose();
+				//    _dataContext = null;
+				//}
+			}
+
+			disposed = true;
+		}
+
+		#endregion
+	}
+}
